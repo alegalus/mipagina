f1eb1f9744812aa1a72f31f9311600948d438604
alegalus <alejandrogalus83@yahoo.com.ar>
2021-01-11 11:09:28 -0300

    Modificacion de eliminacion de imagen de vue js

--- App.js
+++ App.js
@@ -1,4 +1,17 @@
-"use strict"
-alert ("bienvenidos a mi pagina");
+/*window.onload = iniciar
 
-console.log ("prueba");
+function iniciar(){
+        let btnSend = document.getElementById("btnSend")//con esto traigo el elemento del html, no el valor
+        btnSend.addEventListener("click", clickBtnSend)
+    }
+    
+    function clickBtnSend(){
+        let txtName = document.getElementById("txtName")// con esto obtengo los datos del html
+        let name = txtName.value//con esto el valor que se coloque
+        let txtEmail = document.getElementById("txtEmail")
+        let emal = txtEmail.value
+        let txtMessage = document.getElementById("txtMessage")
+        let message= txtMessage.value
+        alert("hola")
+    }*/
+    
